ccaf4499a79f9dac20e123a44c3467d8232aba63
Poulhouse <karpenko.p.s@gmail.com>
2021-11-11 22:08:25 +0300

    cache

--- www/bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php
+++ www/bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001636546466';
-$dateexpire = '001639138466';
+$datecreate = '001636657626';
+$dateexpire = '001639249626';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";N;}';
 return true;
 ?>
